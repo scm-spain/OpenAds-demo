7edebb190e138d1cda14d71ef6b2331a1d6bb090
Victor Caldentey <victor.caldentey@scmspain.com>
2018-10-09 10:11:08 +0200

    LightHouse performance cases
    * Add performance task to build all possible cases
    * Add initial js logic for every case
    * Add performance base template for all cases
    * Add Webpack configuration for performance cases

--- src/index.js
+++ src/index.js
@@ -29,4 +29,40 @@ openAds.addPosition({
   },
   sizes: [[300, 250], [320, 250]]
 })
+  .then(position => openAds.displayPosition({id: position.id}))
 
+openAds.addPosition({
+  id: 'ad2',
+  name: 'ad number one',
+  source: 'AppNexus',
+  placement: 'es-cn-wph-ocasion-list-x_65',
+  segmentation: {
+    'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
+    'es-sch-event_name': 'list',
+    'aa-sch-country_code': 'es',
+    'aa-sch-supply_type': 'wph',
+    'es-sch-section': 'ocasion',
+    'aa-sch-page_type': 'list',
+    'es-sch-adformat': 'x65'
+  },
+  sizes: [[300, 250], [320, 250]]
+})
+  .then(position => openAds.displayPosition({id: position.id}))
+
+openAds.addPosition({
+  id: 'ad3',
+  name: 'ad number one',
+  source: 'AppNexus',
+  placement: 'es-cn-wph-ocasion-list-x_65',
+  segmentation: {
+    'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
+    'es-sch-event_name': 'list',
+    'aa-sch-country_code': 'es',
+    'aa-sch-supply_type': 'wph',
+    'es-sch-section': 'ocasion',
+    'aa-sch-page_type': 'list',
+    'es-sch-adformat': 'x65'
+  },
+  sizes: [[300, 250], [320, 250]]
+})
+  .then(position => openAds.displayPosition({id: position.id}))
