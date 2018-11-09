3fc15486ea84ef72b9b35c24a30e3c31666a0903
alextremp <alex.castells@schibsted.com>
2018-11-09 14:32:35 +0100

    Update Ad calls

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import AppNexusConnector from '@schibstedspain/openads-appnexus'
 
 const appNexusConnector = AppNexusConnector.init({
   config: {
-    member: 3397
+    pageOpts: {
+      member: 3296
+    }
   }
 })
 
@@ -15,54 +17,71 @@ const openAds = OpenAds.init({config:{
 
 openAds.addPosition({
   id: 'ad1',
-  name: 'ad number one',
-  source: 'AppNexus',
-  placement: 'es-cn-wph-ocasion-list-x_65',
-  segmentation: {
-    'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
-    'es-sch-event_name': 'list',
-    'aa-sch-country_code': 'es',
-    'aa-sch-supply_type': 'wph',
-    'es-sch-section': 'ocasion',
-    'aa-sch-page_type': 'list',
-    'es-sch-adformat': 'x65'
-  },
-  sizes: [[300, 250], [320, 250]]
+  name: 'med-rect',
+  specification: {
+   source: 'AppNexus',
+   appnexus: {
+     targetId: 'ad1',
+     invCode: 'es-cn-wph-ocasion-list-x_65',
+     keywords: {
+         'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
+         'es-sch-event_name': 'list',
+         'aa-sch-country_code': 'es',
+         'aa-sch-supply_type': 'wph',
+         'es-sch-section': 'ocasion',
+         'aa-sch-page_type': 'list',
+         'es-sch-adformat': 'x65'
+     },
+     sizes: [[300, 250], [320, 250]]
+   }
+  }
 })
   .then(position => openAds.displayPosition({id: position.id}))
 
 openAds.addPosition({
   id: 'ad2',
-  name: 'ad number one',
-  source: 'AppNexus',
-  placement: 'es-cn-wph-ocasion-list-x_65',
-  segmentation: {
-    'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
-    'es-sch-event_name': 'list',
-    'aa-sch-country_code': 'es',
-    'aa-sch-supply_type': 'wph',
-    'es-sch-section': 'ocasion',
-    'aa-sch-page_type': 'list',
-    'es-sch-adformat': 'x65'
-  },
-  sizes: [[300, 250], [320, 250]]
+  name: 'med-rect',
+  specification: {
+    source: 'AppNexus',
+    appnexus: {
+      targetId: 'ad2',
+      invCode: 'es-cn-wph-ocasion-list-x_65',
+      keywords: {
+          'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
+          'es-sch-event_name': 'list',
+          'aa-sch-country_code': 'es',
+          'aa-sch-supply_type': 'wph',
+          'es-sch-section': 'ocasion',
+          'aa-sch-page_type': 'list',
+          'es-sch-adformat': 'x65'
+      },
+      sizes: [[300, 250], [320, 250]]
+    }
+  }
 })
   .then(position => openAds.displayPosition({id: position.id}))
 
 openAds.addPosition({
   id: 'ad3',
-  name: 'ad number one',
-  source: 'AppNexus',
-  placement: 'es-cn-wph-ocasion-list-x_65',
-  segmentation: {
-    'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
-    'es-sch-event_name': 'list',
-    'aa-sch-country_code': 'es',
-    'aa-sch-supply_type': 'wph',
-    'es-sch-section': 'ocasion',
-    'aa-sch-page_type': 'list',
-    'es-sch-adformat': 'x65'
-  },
-  sizes: [[300, 250], [320, 250]]
+  name: 'med-rect',
+  specification: {
+    source: 'AppNexus',
+    appnexus: {
+      targetId: 'ad3',
+      invCode: 'es-cn-wph-ocasion-list-x_65',
+      keywords: {
+        'es-sch-ads_name_page': 'cochesnet/ocasion/listado',
+        'es-sch-event_name': 'list',
+        'aa-sch-country_code': 'es',
+        'aa-sch-supply_type': 'wph',
+        'es-sch-section': 'ocasion',
+        'aa-sch-page_type': 'list',
+        'es-sch-adformat': 'x65'
+      },
+      sizes: [[300, 250], [320, 250]]
+    }
+  }
 })
   .then(position => openAds.displayPosition({id: position.id}))
+
+setInterval(() => openAds.refreshPosition({id: 'ad3'}), 10000)
